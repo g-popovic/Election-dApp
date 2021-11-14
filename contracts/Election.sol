af26a981a1f274266c5cf9b0d0dfe75d7a75b186
George Popovic <georgepopovich14@gmail.com>
2021-11-14 13:47:26 +0100

    Set up folder structure

---
+++ contracts/Election.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.10;
+
+contract Election {
+    string public candidate;
+
+    constructor() {
+        candidate = "Bill Clinton";
+    }
+}
